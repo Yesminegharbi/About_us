8840fffcedac3652af4b9747f58cc95c58f2406f
Yesminegharbi <yesmine.gharbi@supcom.tn>
2021-12-17 14:20:09 +0100

    last changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,11 +53,11 @@ const images = [
 
                     <div className="box3">
                     <FadeIn>
-                        <div className='image-1'>
+                        <div  className='image-1'>
                         <div className='desc'> 
                         <p>  First aid kit</p>
                         </div>
-                        <img src={'https://images.unsplash.com/photo-1624638764471-cffef5035746?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZpcnN0JTIwYWlkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'} />
+                        <img style={{borderRadius:"130px"}} src={'https://images.unsplash.com/photo-1624638764471-cffef5035746?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZpcnN0JTIwYWlkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'}  />
                         </div>
                       
                       
@@ -66,14 +66,14 @@ const images = [
                         <div className='desc'> 
                         <p>  Don't forget about lightening</p>
                         </div>
-                        <img src={'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FtcGluZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'} />
+                        <img style={{borderRadius:"130px"}} src={'https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FtcGluZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'} />
                         </div>
                         
                         <div className='image-3'>
                         <div className='desc'> 
                         <p>  See more </p>
                         </div>
-                        <img src={'https://images.unsplash.com/photo-1496545672447-f699b503d270?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'}  />
+                        <img style={{borderRadius:"130px"}} src={'https://images.unsplash.com/photo-1496545672447-f699b503d270?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'}  />
                         </div>
                         
                     </FadeIn>
